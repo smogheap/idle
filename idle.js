63a2071f7e0df7ecac59cef9f8696d26cf0b7b55
Micah N Gorrell <github@minego.net>
2013-10-21 17:57:38 -0600

    Tweaked the position of the game on screen a bit

--- idle.js
+++ idle.js
@@ -25,7 +25,7 @@ function IdleEngine(canvas)
 	this.tileSize			= [ 32, 16 ];
 
 	/* Offset for rendering in the middle of the screen */
-	this.offset				= [ 160, 55 ];
+	this.offset				= [ 160, 70 ];
 
 	this.canvas.engine		= this;
 
